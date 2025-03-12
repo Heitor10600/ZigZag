633af688ad870c10080fd3ee2c768c0796281fcd
Heitor de Moura <heitor10600@gmail.com>
2025-03-12 20:00:59 -0300

    .

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="format.css">
+    <link rel="stylesheet" href="responsivo.css">
+    <link rel="icon" href="desing/images/MiniLogo.png" type="image/x-icon">
+    <title>ZigZag</title>
+</head>
+
+<body>
+
+    <header class="top">
+        <img class="logo_header" src="desing/svg/logo.svg" width="90" height="90">
+        <nav class="nav_header">
+            <ul class="ul_header">
+                <li><a href="cadastro.php"> Seja afiliado</a>
+                </li>
+                <li><a href="cadastro.php"> login </a>
+                </li>
+                <li><a class="button_cadastro" href="cadastro.php"> Cadastra-se </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
